6ae9daa78a9281f4f75867a4e28075885dfca08e
Andrew Waite <andrew.waite@ensono.com>
2023-01-10 17:25:09 +0000

    Adding additional functions

---
+++ Src/Functions/Get-A0UserByEmail.Tests.ps1
@@ -0,0 +1,77 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+
+# // START test setup //
+
+$malformedURL = "http:/url.com"
+$formedURL = "http://url.com"
+
+$testExceptionMessage = "test exception"
+
+$testEmail = "test.test@test.com"
+
+. "$here\Private\New-ExceptionDetail.ps1"
+
+# // END test setup //
+
+
+
+Describe "Get-A0UserByEmail" {
+
+    Mock -CommandName Invoke-WebRequest -Verifiable -MockWith {@{}}
+
+    Mock -CommandName New-ExceptionDetail
+    
+    Context "Parameter validation fails" {        
+
+        It "should validate malformed URL and throw" {
+
+            {Get-A0UserByEmail -baseURL $malformedURL -email $testEmail } | Should Throw
+
+            Assert-MockCalled -CommandName Invoke-WebRequest -Exactly -Times 0
+            Assert-MockCalled -CommandName New-ExceptionDetail -Exactly -Times 0
+        }
+
+        It "should validate key missing and throw" {
+
+            {Remove-A0User -baseURL $formedURL -email} | Should Throw
+
+            Assert-MockCalled -CommandName Invoke-WebRequest -Exactly -Times 0
+            Assert-MockCalled -CommandName New-ExceptionDetail -Exactly -Times 0
+        }
+
+
+    }
+
+    Context "Call Auth0" {
+
+        It "should Invoke-WebRequest and return response" {
+
+            Get-A0UserByEmail -baseURL $formedURL -email $testEmail | Should not be $null
+
+            Assert-VerifiableMocks
+        }
+
+    }
+
+    Context "Call Auth0 and fail" {
+
+        Mock -CommandName Invoke-WebRequest -Verifiable -MockWith {
+        
+            Throw
+        }
+
+        It "should throw exception" {
+
+            {Get-A0UserByEmail -baseURL $formedURL -email $testEmail} | Should Throw
+
+        }
+
+        Assert-VerifiableMocks
+        Assert-MockCalled -CommandName New-ExceptionDetail -Exactly -Times 1
+
+
+    }
+}
---
+++ Src/Functions/Get-A0UserByEmail.ps1
@@ -0,0 +1,56 @@
+﻿Function Get-A0UserByEmail {
+	
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory=$false)]
+        [hashtable]$headers = @{"content-type" = "application/json"},
+
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({            
+            If ([uri]::IsWellFormedUriString($_,[urikind]::Absolute)) {Return $true}
+        })]
+        [string]$baseURL,
+
+        [Parameter(Mandatory=$false)]
+        [string]$apiVersion = "v2",
+
+        [Parameter(Mandatory=$true)]
+        [string]$email
+    )
+    
+    Write-Host ("Running function: {0}" -f $MyInvocation.MyCommand.Name) -ForegroundColor Yellow
+
+
+    # // building path //
+    $path = ("api/{0}/users-by-email?email={1}" -f $apiVersion, $email.ToLower())
+
+
+    # // using splatting //
+    $params = @{
+
+        "uri" = "{0}/{1}" -f $baseURL, $path 
+        "method" = "GET"
+
+        "headers" = $headers
+
+    }
+
+    Write-Verbose ("Parameters:`n{0}`nHeaders:`n{1}" -f ($params | Out-String), ($params.headers | Out-String))
+
+    
+    # //sending request //
+    Try {
+        
+        $response = Invoke-WebRequest @params -UseBasicParsing -ErrorAction Stop
+    }
+    
+    Catch {
+
+        New-ExceptionDetail -exception $_ -parameters $params
+
+        Throw $_.Exception
+    }
+
+    Return $response
+}
